f28b9d4c928fde9abd8400216776ad3eeb672543
yv073 <yv073@naver.com>
2026-03-26 00:49:38 +0900

    feat: delete button added

--- mini_list.js
+++ mini_list.js
@@ -1,23 +1,37 @@
 const todoList = document.querySelector("#todoList");
 const addButton = document.querySelector("#addBtn");
+const todoInput = document.querySelector("#todoInput");
+const deleteButton = document.querySelector("#deleteButton");
+
 addButton.addEventListener("click", () => {
   const inputContent = handleContent();
   if (inputContent) {
-    const listItem = document.createElement("li");
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "-";
-    deleteButton.classList.add("deleteButton");
-    listItem.textContent = `${todoInput.value}`;
-    listItem.append(deleteButton);
+    const listItem = createListItem(inputContent);
     todoList.append(listItem);
+    todoInput.value = "";
   }
 });
+const createListItem = (inputContent) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = `${inputContent}`;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "-";
+  deleteButton.classList.add("deleteButton");
+  listItem.append(deleteButton);
+  return listItem;
+};
 
 const handleContent = () => {
-  const todoInput = document.querySelector("#todoInput");
   const trimmed = todoInput.value.trim();
   if (!trimmed) {
     return null;
   }
   return trimmed;
 };
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("deleteButton")) {
+    const listItem = e.target.closest("li");
+    listItem.remove();
+  }
+});
