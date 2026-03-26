ed715857c21c6865447e82faff0d0a903bab888c
yv073 <yv073@naver.com>
2026-03-26 16:59:14 +0900

    feat: check button

--- mini_list.js
+++ mini_list.js
@@ -1,7 +1,8 @@
 const todoList = document.querySelector("#todoList");
 const addButton = document.querySelector("#addBtn");
 const todoInput = document.querySelector("#todoInput");
-const deleteButton = document.querySelector("#deleteButton");
+const deleteButton = document.querySelector(".deleteButton");
+const checkBox = document.querySelector(".checkBox");
 
 addButton.addEventListener("click", () => {
   const inputContent = handleContent();
@@ -13,12 +14,16 @@ addButton.addEventListener("click", () => {
 });
 const createListItem = (inputContent) => {
   const listItem = document.createElement("li");
+  const deleteButton = document.createElement("button");
+  const checkBox = document.createElement("button");
+
   listItem.textContent = `${inputContent}`;
 
-  const deleteButton = document.createElement("button");
   deleteButton.textContent = "-";
   deleteButton.classList.add("deleteButton");
+  checkBox.classList.add("checkBox");
   listItem.append(deleteButton);
+  listItem.append(checkBox);
   return listItem;
 };
 
@@ -33,5 +38,18 @@ todoList.addEventListener("click", (e) => {
   if (e.target.classList.contains("deleteButton")) {
     const listItem = e.target.closest("li");
     listItem.remove();
+    return;
+  }
+  if (e.target.classList.contains("checkBox")) {
+    isDone(e.target);
+    return;
   }
 });
+const isDone = (target) => {
+  target.classList.toggle("done");
+  if (target.classList.contains("done")) {
+    target.textContent = "✓";
+  } else {
+    target.textContent = "";
+  }
+};
